9f2a481407c19252941e40c0c28407314307847c
Roman Rudyak <roma.rudyak@gmail.com>
2017-11-23 17:34:01 +0200

    Configured Xam XMPP project with forms

---
+++ Xam.XMMP/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xam.XMMP
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ContentPage { Content = new Label { Text = "Hello RORU!" } };
+        }
+
+    }
+}
---
+++ Xam.XMPP.Droid/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using Xam.XMMP;
+
+namespace Xam.XMPP.Droid
+{
+    [Activity(Label = "Xam.XMPP.Droid", MainLauncher = true, Theme = "@style/MyTheme")]
+    public class MainActivity : FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            FormsAppCompatActivity.ToolbarResource = Resource.Layout.Toolbar;
+            FormsAppCompatActivity.TabLayoutResource = Resource.Layout.Tabbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Forms.Init(this, savedInstanceState);
+
+            LoadApplication(new App());
+        }
+    }
+}
+
