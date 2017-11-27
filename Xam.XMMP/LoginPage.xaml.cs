0c7e4c39a7b3987d06a943bc133b6986323c0bc3
Roman Rudyak <roma.rudyak@gmail.com>
2017-11-27 12:26:51 +0200

    Add login form with data

--- Xam.XMMP/LoginPage.xaml.cs
+++ Xam.XMMP/LoginPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sharp.Xmpp.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,13 @@ namespace Xam.XMMP
 
         public void Login_Click(object sender, EventArgs args)
         {
+            var login = _login.Text;
+            var pass = _pass.Text;
 
+            using (var client = new XmppClient("jabber.to", login, pass))
+            {
+                client.SendMessage(new Sharp.Xmpp.Im.Message(new Sharp.Xmpp.Jid("xam.jab@jabber.to"), "Hi xam!!"));
+            }
         }
 	}
 }
