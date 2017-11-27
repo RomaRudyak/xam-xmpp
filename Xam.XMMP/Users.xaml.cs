a0f38b641e1a97db3085df40a312880cf7b6d288
Roman Rudyak <roma.rudyak@gmail.com>
2017-11-27 17:49:03 +0200

    Add users list of client

---
+++ Xam.XMMP/Users.xaml.cs
@@ -0,0 +1,41 @@
+﻿using SharpXMPP.XMPP.Client.Roster.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xam.XMMP
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Users : ContentPage
+	{
+        private readonly SharpXMPP.XmppClient _xmppconnetction;
+
+        public Users()
+        {
+            InitializeComponent();
+
+        }
+
+        public Users (SharpXMPP.XmppClient connection)
+        {
+            InitializeComponent();
+            _xmppconnetction = connection;
+            _users.ItemsSource = connection.RosterManager.Roster;
+        }
+
+        public void RosterSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return;
+            }
+            var rester = (RosterItem)e.SelectedItem;
+            Navigation.PushModalAsync(new Chat(rester));
+        }
+    }
+}
