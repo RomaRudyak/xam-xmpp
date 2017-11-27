e5b87aef14cfec05477b413c0863c71a3f4c1898
Roman Rudyak <roma.rudyak@gmail.com>
2017-11-27 17:47:36 +0200

    Fix navigation to user list

--- Xam.XMMP/LoginPage.xaml.cs
+++ Xam.XMMP/LoginPage.xaml.cs
@@ -12,43 +12,25 @@ using Xamarin.Forms.Xaml;
 
 namespace Xam.XMMP
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class LoginPage : ContentPage
-	{
-        private string _domain = "@chinwag.im";
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
 
 
-        public LoginPage ()
-		{
-			InitializeComponent ();
-		}
-
-
-        public void Login_Click(object sender, EventArgs args)
+        public async void Login_Click(object sender, EventArgs args)
         {
             var login = _login.Text;
             var pass = _pass.Text;
 
-            var client = new XmppClient(new SharpXMPP.XMPP.JID($"{login}@{_domain}"), pass);
-            client.Connect();
-            client.ConnectionFailed += Client_ConnectionFailed;
-            client.SignedIn += Client_SignedIn;
-        }
-
-        private void Client_SignedIn(XmppConnection sender, SignedInArgs e)
-        {
-            sender.Send(new XMPPMessage() {
-                Text = "Hello RORU!",
-                To = e.Jid
-            });
-        }
-
-        private void Client_ConnectionFailed(XmppConnection sender, ConnFailedArgs e)
-        {
-            UserDialogs.Instance.Alert(new AlertConfig
-            {
-                Message = e.Message
-            });
+            IsBusy = true;
+            var client = new XmppClient(new SharpXMPP.XMPP.JID(login), pass);
+            await client.Connect();
+            IsBusy = false;
+            await Navigation.PushModalAsync(new Users(client));
         }
     }
 }
